9ecbcdb8929e35e9bba480273597ba4d420fe76b
Linford Kennedy Amankwaa <102173674+Bantama@users.noreply.github.com>
2022-07-13 15:25:50 +0000

    I worked on challenge 1 & 2. I added a 'for loop' that runs for the length of an array to both codes and personalized the variables. I also removed the comment after the function.

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+var sum = 0;
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+    sum = sum + arrayOfNumbers[index];
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +32,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+var even=0;
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+    if (arrayOfNumbers[index]%2==0) {
+      even++
+    }
+  }
+  
+    return even;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,7 +60,11 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+var degree=[]
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+    degree.push((arrayOfNumbers[index] * 9/5) + 32)
+  }
+    return degree;
 }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
